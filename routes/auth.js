f43b6bb64c23a46f1d80b1b0d08c3b5797a68608
middleware <malifarge.thomas9@gmail.com>
2022-11-02 12:19:55 +0100

    passport

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
-// const issueToken = require('../utils/jwt')
+const issueToken = require('../utils/jwt')
 const { User } = require('../models/index')
 
 app.post('/signup', async (req, res) => {
@@ -21,6 +21,34 @@ app.post('/signup', async (req, res) => {
 
 app.get('/signup', (req, res) => {
   res.json('hello')
+})
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  console.log(User);
+
+  const user = await User.findOne({
+    where: {
+      email
+    }
+  })
+
+  if (!user) {
+    res.status(404).send('Not found')
+  } else {
+    const validPassword = await bcrypt.compare(password, user.password)
+
+    if (validPassword) {
+      const token = issueToken({ id: user.id, email: user.email })
+
+      res.json({
+        token
+      })
+    } else {
+      res.status(404).send('Not found')
+    }
+  }
 })
 
 module.exports = app
