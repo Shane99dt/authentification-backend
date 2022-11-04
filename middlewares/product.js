ede310fb9725b0aa582499976f3f7e7fbcb6dd7e
Shane99dt <dushen1999thilaksha@gmail.com>
2022-11-04 15:50:33 +0100

    erase console

--- middlewares/product.js
+++ middlewares/product.js
@@ -2,14 +2,11 @@ const { Product } = require("../models")
 
 const checkIfProductExists = async (req, res, next) => {
   const { productId } = req.params
-  console.log(productId)
-
   const product = await Product.findOne({
     where: {
       id: productId,
     },
   })
-  console.log(product)
 
   if (product) {
     req.product = product
